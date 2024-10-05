5faadb064a9097d0a14b1048e1ff1c16e48f9557
Jorandelgado23 <Jorandelgado23@gmail.com>
2024-10-05 19:21:17 +0800

    updated

--- beneficiary_attendance.php
+++ beneficiary_attendance.php
@@ -1,6 +1,296 @@
 <?php
-// Start session and check user login status
 session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("Location: login.php"); // Redirect to login if not logged in
+    exit();
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sbfp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_SESSION['email'];
+
+// Prepare and bind
+$stmt = $conn->prepare("SELECT firstname, lastname, school_name, role FROM users WHERE email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($user_firstname, $user_lastname, $school_name, $user_role);
+
+if ($stmt->num_rows > 0) {
+    $stmt->fetch();
+    if (!isset($_SESSION['welcome_shown'])) {
+        $welcome_message = "Welcome, $user_firstname $user_lastname!";
+        $_SESSION['welcome_shown'] = true; // Set the session variable
+    }
+} else {
+    echo "No user found with that email address.";
+    exit();
+}
+
+$stmt->close();
+$conn->close();
+?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>MONTHLY/QUARTERLY REPORT</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- site icon -->
+      <link rel="icon" href="images/LOGO.png" type="image/png" />
+
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <!-- site css -->
+      <link rel="stylesheet" href="style.css" />
+      <!-- responsive css -->
+      <link rel="stylesheet" href="css/responsive.css" />
+      <!-- color css -->
+      <link rel="stylesheet" href="css/color_2.css" />
+      <!-- select bootstrap -->
+      <link rel="stylesheet" href="css/bootstrap-select.css" />
+      <!-- scrollbar css -->
+      <link rel="stylesheet" href="css/perfect-scrollbar.css" />
+      <!-- custom css -->
+      <link rel="stylesheet" href="css/custom.css" />
+
+      
+
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <style>
+    .active {
+        background-color: lightblue; /* Background color */
+        color: #fff; /* Text color */
+        font-weight: bold; /* Bold text */
+    }
+    
+</style>
+
+<body class="dashboard dashboard_2">
+    <div class="full_container">
+        <div class="inner_container">
+            <!-- Sidebar -->
+            <nav id="sidebar">
+                <div class="sidebar_blog_1">
+                    <div class="sidebar-header">
+                        <div class="logo_section">
+                            <a href="dashboard.php"><img class="logo_icon img-responsive" src="images/logo/semilogo.png" alt="#" /></a>
+                        </div>
+                    </div>
+                    <div class="sidebar_user_info">
+    <div class="icon_setting"></div>
+    <div class="user_profle_side">
+        <div class="user_img"><img class="img-responsive" src="images/origlogo.jpg" alt="#" /></div>
+        <div class="user_info">
+            <h6><?php echo $school_name; ?></h6> <!-- Display school name here -->
+            <p><span class="online_animation"></span> Online</p>
+        </div>
+    </div>
+</div>
+
+
+                </div>
+                <div class="sidebar_blog_2">
+                    <h4>General</h4>
+                    <ul class="list-unstyled components">
+                    <li>
+                            <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>DASHBOARD</span></a>
+                        </li>
+
+                        <li>
+                            <a href="form1.php"><i class="fa fa-group"></i> <span>Master List Of Student</span></a>
+                        </li>
+
+                        <li>
+                            <a href="Beneficiary_list.php"><i class="fa fa-line-chart"></i> <span>Beneficiary Improvement</span></a>
+                        </li>
+
+                        <li>
+                            <a href="progress_input.php"><i class="fa fa-pencil-square"></i> <span>Progress Input</span></a>
+                        </li>
+                        <li>
+                            <!-- <a href="form2.php"><i class="fa fa-file-excel-o"></i> <span>SBFP-FORM 2</span></a> -->
+                        </li>
+                        <!-- <li>
+                            <a href="form3.php"><i class="fa fa-file-excel-o"></i> <span>SBFP-FORM 3</span></a>
+                        </li> -->
+                        <li>
+                            <a href="student_attendance.php"><i class="fa fa-calendar"></i> <span>Student Attendance</span></a>
+                        </li>
+                        <!-- <li>
+                            <a href="form5.php"><i class="fa fa-file-excel-o"></i> <span>SBFP-FORM 5</span></a>
+                        </li> -->
+                        <li>
+                            <a href="form6.php"><i class="fa fa-flask"></i> <span>Milk Component Data</span></a>
+                        </li>
+                        <li class="active">
+                            <a href="beneficiary_attendance.php"><i class="fa fa-file-excel-o"></i> <span>beneficiary attendance</span></a>
+                        </li>
+                        <li>
+                            <a href="form8.php"><i class="fa fa-file-text-o"></i> <span>QUARTERLY REPORT</span></a>
+                        </li>
+
+                     
+                      
+                      
+            
+                        
+                        <li>
+                            <a href="usersetting.php"><i class="fa fa-cog yellow_color"></i> <span>Settings</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <!-- End Sidebar -->
+            <!-- Right Content -->
+            <div id="content">
+                <!-- Topbar -->
+                <div class="topbar">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="full">
+                            <button type="button" id="sidebarCollapse" class="sidebar_toggle"><i class="fa fa-bars"></i></button>
+                            <div class="logo_section">
+                            <a href="dashboard.php"><img class="img-responsive" src="images/logo/semilogo.png" alt="#" /></a>
+                            </div>
+                            <div class="right_topbar">
+                                <div class="icon_info">
+                                    <ul>
+                                    <li>
+
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sbfp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+// Query recent activities count with status 'new'
+$sql = "SELECT COUNT(*) AS activity_count FROM sbfp_recent_activity WHERE status = 'new'";
+$result = $conn->query($sql);
+
+// Initialize activity count
+$activity_count = 0;
+
+// Fetch activity count if available
+if ($result && $result->num_rows > 0) {
+$row = $result->fetch_assoc();
+$activity_count = $row['activity_count'];
+}
+
+// Close the connection after query execution
+$conn->close();
+?>
+
+<!-- Notification Icon with Activity Count -->
+<a href="sbfp_activity.php?mark_read=true">
+<i class="fa fa-bell-o"></i>
+<?php if ($activity_count > 0): ?>
+<span class="badge"><?php echo htmlspecialchars($activity_count); ?></span>
+<?php endif; ?>
+</a>
+
+
+                                    </ul>
+                                    <ul class="user_profile_dd">
+                                        <li>
+                                            
+                                        <a class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+       <!-- <img class="img-responsive rounded-circle" src="images/origlogo.jpg" alt="#" /> -->
+
+    <span class="name_user"><?php echo $user_role; ?></span>
+
+</a>
+
+<div class="dropdown-menu">
+                                                <a class="dropdown-item" href="usersetting.php">My Profile</a>
+                                                <a class="dropdown-item" href="#" id="logoutLink">
+    <span>Log Out</span> <i class="fa fa-sign-out"></i>
+</a>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+    document.getElementById('logoutLink').addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent default link action
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out of your account!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log me out!',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Redirect to logout.php if confirmed
+                window.location.href = 'logout.php';
+            }
+        });
+    });
+</script>
+
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <!-- End Topbar -->
+                <!-- Dashboard Inner -->
+                <div class="midde_cont">
+    <div class="container-fluid">
+        <div class="row column_title">
+            <div class="col-md-12">
+                <div class="page_title">
+                    <h2>Beneficiaries Attendance</h2>
+                </div>
+            </div>
+        </div>
+
+      
+        <?php
+
 if (!isset($_SESSION['email'])) {
     // Redirect to login if the user is not logged in
     header("Location: login.php");
@@ -39,15 +329,10 @@ $stmt->execute();
 $result = $stmt->get_result();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>SBFP Attendance System</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
+  
+
 
 <div class="container">
     <h2 class="mt-4">Beneficiary Attendance for SBFP</h2>
@@ -81,9 +366,13 @@ $result = $stmt->get_result();
                     </td>
                     <td>
                         <select name="meal_served[<?= $row['id'] ?>]" class="form-control" required>
-                            <option value="Hot Meal">Hot Meal</option>
-                            <option value="Milk">Milk</option>
-                            <option value="Both">Both</option>
+                            <option value="H">Hot Meal (H)</option>
+                            <option value="M">Milk (M)</option>
+                            <option value="H/M">Hot Meal & Milk (H/M)</option>
+                            <option value="A">Absent (A)</option>
+                            <option value="H2">Hot Meal Twice (H2)</option>
+                            <option value="M2">Milk Twice (M2)</option>
+                            <option value="H/M2">Hot Meal & Milk Twice (H/M2)</option>
                         </select>
                     </td>
                 </tr>
@@ -101,13 +390,9 @@ $result = $stmt->get_result();
 $conn->close();
 ?>
 
-<!-- JS and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-</body>
-</html>
+
+
 
 
 
@@ -157,15 +442,10 @@ $stmt->execute();
 $result = $stmt->get_result();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>View Attendance Records</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
+
+
 
 <div class="container">
     <h2 class="mt-4">Attendance Records</h2>
@@ -209,13 +489,9 @@ $result = $stmt->get_result();
 $conn->close();
 ?>
 
-<!-- JS and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-</body>
-</html>
+
+
 
 
 
@@ -269,15 +545,10 @@ $stmt->execute();
 $result = $stmt->get_result();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Absence Statistics</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
+
+
 
 <div class="container">
     <h2 class="mt-4">Beneficiary Absence Statistics</h2>
@@ -307,13 +578,6 @@ $result = $stmt->get_result();
 $conn->close();
 ?>
 
-<!-- JS and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-</body>
-</html>
 
 
 
@@ -389,18 +653,13 @@ while ($row = $attendance_result->fetch_assoc()) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>SBFP Attendance System</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
+    
+
 
 <div class="container">
-    <h2 class="mt-4">Feeding Attendance Table</h2>
+
 
     <!-- Table Section -->
     <div class="col-md-13">
@@ -474,10 +733,24 @@ while ($row = $attendance_result->fetch_assoc()) {
 $conn->close();
 ?>
 
-<!-- JS and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+
+
+
+
+            </div>
+        </div>
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap Bundle JavaScript -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Custom Scripts -->
+    <script src="js/custom.js"></script>
+    <!-- Chart Plugins -->
+    <script src="js/Chart.min.js"></script>
+    <!-- Init Charts -->
+    
 </body>
+
 </html>
--- submit_attendance.php
+++ submit_attendance.php
@@ -1,5 +1,5 @@
 <?php
-// Start session
+// Start session and check user login status
 session_start();
 
 // Include database connection
@@ -21,6 +21,13 @@ $attendance_date = $_POST['attendance_date'];
 $status = $_POST['status'];
 $meal_served = $_POST['meal_served'];
 
+// Debugging to check if form data is correctly received
+// You can uncomment these lines to test if the form data is properly submitted
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+// exit;
+
 // Loop through each beneficiary
 foreach ($status as $beneficiary_id => $attendance_status) {
     // Get the corresponding meal served
@@ -29,6 +36,9 @@ foreach ($status as $beneficiary_id => $attendance_status) {
     // Check if the attendance record already exists for this beneficiary and date
     $check_sql = "SELECT id FROM beneficiary_attendance WHERE beneficiary_id = ? AND attendance_date = ?";
     $stmt = $conn->prepare($check_sql);
+    if (!$stmt) {
+        die("Error preparing statement: " . $conn->error); // Error handling
+    }
     $stmt->bind_param("is", $beneficiary_id, $attendance_date);
     $stmt->execute();
     $stmt->store_result();
@@ -37,6 +47,9 @@ foreach ($status as $beneficiary_id => $attendance_status) {
         // Record exists, update the existing record
         $update_sql = "UPDATE beneficiary_attendance SET status = ?, meal_served = ? WHERE beneficiary_id = ? AND attendance_date = ?";
         $update_stmt = $conn->prepare($update_sql);
+        if (!$update_stmt) {
+            die("Error preparing update statement: " . $conn->error); // Error handling
+        }
         $update_stmt->bind_param("ssis", $attendance_status, $meal, $beneficiary_id, $attendance_date);
         $update_stmt->execute();
         $update_stmt->close();
@@ -44,6 +57,9 @@ foreach ($status as $beneficiary_id => $attendance_status) {
         // No record exists, insert a new record
         $insert_sql = "INSERT INTO beneficiary_attendance (beneficiary_id, attendance_date, status, meal_served) VALUES (?, ?, ?, ?)";
         $insert_stmt = $conn->prepare($insert_sql);
+        if (!$insert_stmt) {
+            die("Error preparing insert statement: " . $conn->error); // Error handling
+        }
         $insert_stmt->bind_param("isss", $beneficiary_id, $attendance_date, $attendance_status, $meal);
         $insert_stmt->execute();
         $insert_stmt->close();
